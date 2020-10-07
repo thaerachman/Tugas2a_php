10f3da689623ff7051f172679cee12a022b7957f
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 15:36:14 +0700

    Add files via upload

---
+++ tugas_2_a.php
@@ -0,0 +1,16 @@
+<?php
+$ntukang1=24;
+$ntukang2=12;
+$nhari1=36;
+
+// Rumus:
+$nhari= ($ntukang2 * $nhari1) / $ntukang1;
+
+echo "Tugas 2a php <br>";
+echo "------------------";
+echo "<br>";
+echo "Dengan jumlah Pekerja sebanyak ".$ntukang1." orang," ;
+echo "<br>";
+echo "maka rumah akan selesai selama = ".$nhari." hari";
+
+?>
